2da4b44deb7c66cb7b42ec94e4f13b952fa2b007
Jarek Plonski <jaroslaw.plonski@gmail.com>
2012-10-16 17:38:35 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nkf::Application.config.secret_token = '542651a9da2dd0093e3591bef3274b0eb9b7ae90f93641838e7ef323e85c3f10d7e3bf8862388bceb4d18368fb8ec47a321f0701411faa3d30f3fd5dcde9fd2c'
